21f20ffea1b265be33f57e3cbd8030f02ff59cde
Charith <charithmaddoju27@gmail.com>
2022-09-19 23:30:29 +0530

    Made form

--- script.js
+++ script.js
@@ -14,43 +14,34 @@ function addBookToLibrary(title,author,pages){
     const book = new Book(title,author,pages,true);
     myLibrary.push(book);
 }
-// const theHobbit = new Book("The Hobbit","J.R.R. Tolkien", 295, false)
 
-// console.log(theHobbit.info());
+const addbook = document.querySelector('.addbook');
+const form = document.querySelector('.form')
+const formTitle = document.querySelector('.form-title');
+const formAuthor = document.querySelector('.form-author');
+const formPages = document.querySelector('.form-pages');
+const formSubmit = document.querySelector('.form-submit');
+const bookshelf = document.querySelector('.bookshelf');
 
-const addbook = document.querySelector('.addbook')
-const booksContainer = document.querySelector('.books-container');
-const form = document.querySelector('.form');
 
-const card = document.createElement('div');
-const cardTitle = document.createElement('div')
-const cardAuthor = document.createElement('div')
-const cardPages = document.createElement('div')
-
-addbook.addEventListener('click',(e) => {
-    e.preventDefault;
+addbook.addEventListener('click', function(e){
     form.style.display = "grid";
 
+    formSubmit.addEventListener('click', function(e){
 
-    const formTitle = document.querySelector('.form-title');
-    const formAuthor = document.querySelector('form-author');
-    const formPages = document.querySelector('form-pages');
-    const formSubmit = document.querySelector('.form-submit');
-    const formReset = document.querySelector('.form-reset');
+        let card = document.createElement('div');
+        let cardTitle = document.createElement('div');
+        let cardAuthor = document.createElement('.div');
+        let cardPages = document.createElement('.div');
 
-    formSubmit.addEventListener('click',(e) => {
-        form.style.display = "none";
-        title = formTitle;
-        author = formAuthor;
-        pages = formPages;
-
-        addBookToLibrary(title,author,pages,true)
-
-        
-        
-
+        cardTitle.innerText = formTitle.value
+        cardAuthor.innerText = formAuthor.value 
+        cardPages.innerText = formPages.value       
     })
+
 })
+
+
 
 
 

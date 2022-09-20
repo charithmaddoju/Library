616d5c094c6fa2c8f491a79743fb52ed84dbecfc
Charith <charithmaddoju27@gmail.com>
2022-09-20 19:19:31 +0530

    Add book title,author,page from form

--- script.js
+++ script.js
@@ -26,33 +26,47 @@ const flag = 0;
 
 addbook.addEventListener('click', function(e){
 
-    
     form.style.display = "grid";
+
+})
 
     formSubmit.addEventListener('click', function(e){
 
+        form.style.display = "none"
         let card = document.createElement('div');
         let cardTitle = document.createElement('div');
         let cardAuthor = document.createElement('div');
         let cardPages = document.createElement('div');
+        let cardRead = document.createElement('button');
 
-        cardTitle.innerText = formTitle.value
+        cardTitle.innerText = `"${formTitle.value}"`
         cardAuthor.innerText = formAuthor.value 
         cardPages.innerText = formPages.value    
+        cardRead.innerText = "Read";
+
+        card.classList.add('card');
 
         cardTitle.classList.add('block');
+        cardTitle.classList.add('card-title');
         cardAuthor.classList.add('block');
+        cardAuthor.classList.add('card-author');
         cardPages.classList.add('block');
+        cardPages.classList.add('card-pages');
+        cardRead.classList.add('block');
+        cardRead.classList.add('card-read');
         
         card.append(cardTitle);
         card.append(cardAuthor);
         card.append(cardPages);
+        card.append(cardRead);
 
         bookshelf.append(card);
         form.style.display = "none"
+
+        formTitle.value = '';
+        formAuthor.value = '';
+        formPages.value = '';
     })
-    
-})
 
 
 
